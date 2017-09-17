486c2fe0793835e66c0c15ad7f917b4844929239
Gal Orlanczyk <gal.orlanczyk@outlook.com>
2017-09-17 16:22:21 +0300

    Added kaltura banners

--- Sources/SynchronizedProperty.swift
+++ Sources/SynchronizedProperty.swift
@@ -1,10 +1,13 @@
+// ===================================================================================================
+// Copyright (C) 2017 Kaltura Inc.
 //
-//  SynchronizedProperty.swift
-//  Pods
+// Licensed under the AGPLv3 license, unless a different license for a 
+// particular library is specified in the applicable library path.
 //
-//  Created by Gal Orlanczyk on 04/09/2017.
-//
-//
+// You may obtain a copy of the License at
+// https://www.gnu.org/licenses/agpl-3.0.html
+// ===================================================================================================
+
 
 import Foundation
 
